aafe9096ab55ccd0b58cf4fb9f8f8b1553330ca5
Amanuel <fikadieamanuel@gmail.com>
2026-07-11 20:32:58 +0100

    configure registration routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react'
+import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Login from './components/login/login.jsx';
+import RegistrationPage from './components/registration/RegistrationPage.jsx';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Login />
-    </>
+     <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<RegistrationPage />} />
+    </Routes>
   )
 }
 
